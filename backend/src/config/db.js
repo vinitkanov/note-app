2fb3117b108119ead89eac83a4b89555b6991ca2
Vinitka's Dinasty <145179274+vinitkanov@users.noreply.github.com>
2025-09-23 11:21:02 +0700

    feat: Add driver for mySQL

---
+++ backend/src/config/db.js
@@ -0,0 +1,20 @@
+import mysql2 from "mysql2/promise";
+
+const pool = mysql2.createPool({
+    host: "localhost",
+    user:"root",
+    password: "",
+    database: "notes_app"
+})
+
+export const testConnection = async () => {
+    try {
+        const connection = await pool.getConnection();
+        console.log("Database connected successfully.");
+        connection.release();
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
+}
+
