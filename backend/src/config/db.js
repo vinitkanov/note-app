92a8eb10e8f472a761b03afdfbc39d25e7de21b7
Vinitka Yeshynovka <your-email@example.com>
2025-10-06 11:55:57 +0700

    setup backend deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,13 @@
 import mysql2 from "mysql2/promise";
 
 export const pool = mysql2.createPool({
-    host: "localhost",
-    user:"root",
-    password: "",
-    database: "notes_app"
+    host: "sql12.freesqldatabase.com",
+    user:"sql12801503",
+    password: "pNE15Seyg8",
+    database: "sql12801503",
+    waitForConnections: true,
+    connectionLimit: 10,    
+    queueLimit: 0
 })
 
 export const testConnection = async () => {
