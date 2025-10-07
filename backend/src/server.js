1be783f1a7291064b7d0f307de27419043bc9547
Vinitka's Dinasty <145179274+vinitkanov@users.noreply.github.com>
2025-10-07 11:18:45 +0700

    fix origin app use CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://note-app-main-eight.vercel.app', 
     credentials: true
 }));
 
